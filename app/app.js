066e73edb02a0dc222bc052e2d9d003093b05934
cesartera <87489244+cesartera@users.noreply.github.com>
2022-03-21 21:11:29 -0300

    Primeiro Commit
    Add arquivos html, css e js

---
+++ app/app.js
@@ -0,0 +1,14 @@
+console.log('Exemplo CRM está funcionando!');
+
+
+var todosProdutos = [
+    {
+    nome:"produto 1",
+    imagem: "link/caminho/imagem-produto1.png",
+    categoria: 'mouse',
+    },
+    {},
+    {},
+]
+
+bucarProduto(mouse);
